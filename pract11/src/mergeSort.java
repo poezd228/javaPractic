8eb5cf17d3056731213d9ebe0f87728e2542cc0b
vlaD <sant.vladislav@gmail.com>
2023-10-17 21:40:51 +0300

    new version

--- pract11/src/mergeSort.java
+++ pract11/src/mergeSort.java
@@ -1,11 +1,11 @@
 public class mergeSort extends QuickSort{
 
-    public void mergeSort(int[] arr, int l, int r) {
+    public void MergeSort(int[] arr, int l, int r) {
         if (l < r) {
             int m = (l + r) / 2;
 
-            mergeSort(arr, l, m);
-            mergeSort(arr, m + 1, r);
+            MergeSort(arr, l, m);
+            MergeSort(arr, m + 1, r);
 
             merge(arr, l, m, r);
         }
